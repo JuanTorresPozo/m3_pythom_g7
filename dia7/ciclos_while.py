501b7cb68974340b419bbd5fa594b97e0cd2ee00
Juan Torres <juantorresp@gmail.com>
2024-03-20 23:57:30 -0300

    dia7

--- dia7/ciclos_while.py
+++ dia7/ciclos_while.py
@@ -1,19 +1,21 @@
 """
  while condicion:   
-    
+    #codigo a ejecutar
      
 """
 #import getpass
 """
-password = input("Ingrese su contraseña:\n")
+password = getpass.getpass("Ingrese su contraseña:\n")
 
+while len(password) < 6
+    password = input("Ingrese su contraseña con largo superior o igual a 6 caracteres:\n")
 
 while password != "Hola Mundo":
-    password = input("no adivinaste la contraseña, Ingrese su contraseña nuevamente:\")
+    password = getpass.getpass ("no adivinaste la contraseña, Ingrese su contraseña nuevamente:\n")
                      
 print("\nContraseña correcta")
-#resto del programa
-print("Fin del programa")
+#resto del programa 
+print ("Fin del programa")
 """
 
 
@@ -24,11 +26,14 @@ while i < 10:
     i+=1 #incremento en 1 => i = i +1
     
 print(f"fuera del while, el valor de i= {i}")
+
 """
 while True:
-    print("acciones")
+    print("acciones infinitas")
     if condicion:
     else:
         break #salir del bucle
+"""       
+"""
 """
     
