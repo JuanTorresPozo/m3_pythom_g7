dee5d0efa11bed41c1f4afb0ec82d11aa4331954
Juan Torres <juantorresp@gmail.com>
2024-03-22 00:45:30 -0300

    Actualizado

--- dia8/search.py
+++ dia8/search.py
@@ -1,6 +1,6 @@
-import sys
+import sys, random
 
-numero_buscar = int (sys.arg[1])
+numero_buscar = int (sys.argv[1])
 
 lista = [1,2,3,4,5,6,7,8,9,0]
 random.shuffle(lista) #desordena la lista, permanente
@@ -12,6 +12,6 @@ for posicion, numero in enumerate(lista):
         print("Numero encontrado")
         break
     else: 
-        print (f"El elemento no se encuentra en la posicion {posicion}\n")
+        print (f"El numero no se encuentra en la posicion {posicion}\n")
 print ("Fuera del for")
 print(f"El numero {numero_buscar} se encuantra en la posicion {posicion}")
