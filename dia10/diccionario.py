6adab04c0e276982a0bbb7f6c55c962e879679ce
Juan Torres <juantorresp@gmail.com>
2024-03-26 11:17:53 -0300

    clase dia 11

--- dia10/diccionario.py
+++ dia10/diccionario.py
@@ -6,6 +6,8 @@
     las claves no se generan automaticamente, no hay un orden
     las claves pueden ser String, numeros enteros incluso boolean
     Los balores pueden ser numericos boolean, listas, diccionarios, otro tipo de datos
+    Si la clave existe modifica su valor , si la clave no existe la crea
+    
 """
 diccionario = {
     1:"uno", 
@@ -37,3 +39,36 @@ print(notas["Felipe"])
 print(notas["Daniela"])
 print(notas["Vicente"])
 #print(notas["felipe"])#KeyError: 'felipe'
+
+print(len(notas))
+
+
+print(notas)#{'Camila': 7, 'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 7}
+notas["julio"] = 4
+print(notas)#{'Camila': 7, 'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 7, 'julio': 4}
+
+#julio = {"nota1" : 7, "nota2" : 4}
+
+# Modificar par clave:valor
+notas["julio"] = 5
+{'Camilaprint(notas)#': 7, 'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 7, 'julio': 5}
+
+#Eliminar  par clave:valor
+#del notas ["FELIPE"]
+print(notas)#{'Camila': 7, 'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 7, 'julio': 5}
+#2 forma pop() -> al eliminar, capturamos el valor
+eliminado = notas.pop("Camila")
+print("valor eliminado : ",eliminado)#7
+print(notas)#{'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 7, 'julio': 5}
+
+notas2 = {
+    "Mijail":2,
+    "Israel":1
+    
+}
+
+notas.update(notas2)
+print(notas)#{'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 7, 'julio': 5, 'Mijail': 2, 'Israel': 1}
+notas2.update(notas)
+print(notas2)#
+#COLICIONES Al existir duplicidadde claves, se conserva
---
+++ dia11/ejercicio1.py
@@ -0,0 +1,18 @@
+#Crear diccionario
+notas ={
+    "Juan":7,
+    "Pedro":5,
+    "Maria":6,
+    "Jose":3
+}
+print(notas)
+
+#Agregar elemento
+notas["Lucia"] = 4
+print(notas)
+
+#Modificar elemento
+notas["Pedro"] = 6
+print(notas)
+
+#Eliminar elemento
