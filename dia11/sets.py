173a2e72df8bbd6ac9dfdb4b9e2a5b9782940b3e
Juan Torres <juantorresp@gmail.com>
2024-03-26 13:10:06 -0300

    Clase 11

---
+++ dia11/sets.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+muchos_animales = {'Gato', 'Perro', 'Tortuga',
+'Gato', 'Perro', 'Tortuga',
+'Gato', 'Perro', 'Tortuga',
+'Gato', 'Perro', 'Tortuga',
+'Hurón', 'Hamster', 'Erizo de Tierra'}
+
+print(muchos_animales)
+
+for elemento in muchos_animales:
+    print(elemento)
